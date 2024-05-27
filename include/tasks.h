2b10116fbede0c949045de5617285766973294b7
SmDar1a <Smirnova.darya.vl@gmail.com>
2024-05-27 19:10:34 +0300

    second lab

---
+++ include/tasks.h
@@ -0,0 +1,10 @@
+// Copyright 2024 Smirnova Darya
+
+#pragma once
+#include "circle.h"
+double EarthAndRopeTask(double rope_length);
+double SwimmingPoolTask(double pool_radius_meter,
+    double road_width_meter,
+    double concrete_price,
+    double fence_price);
+    
